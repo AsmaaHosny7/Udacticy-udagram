fe4efb6c5c2d7dc64f4d809b49038c41c72f7829
Asmaa Hosny <49831110+AsmaaHosnyAly@users.noreply.github.com>
2022-06-27 20:53:02 +0200

    ...

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,24 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.cryuatw8x1kt.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=s3://asmaa-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
+
+eb setenv  POSTGRES_USERNAME="postgres"\
+    # POSTGRES_PASSWORD = "postgres"\
+    # POSTGRES_DB="postgres"\
+    # POSTGRES_DB_PORT="5432"\
+    # POSTGRES_HOST="database-1.cryuatw8x1kt.us-east-1.rds.amazonaws.com"\
+    # AWS_REGION="us-east-1"\
+    # AWS_PROFILE="default"\
+    # AWS_BUCKET="s3://asmaa-udagram"\
+    # AWS_SECRET_ACCESS_KEY="E2DP93it4VES+nX9d2SSshwfz7Tko20f2oOiyD03"\
+    # AWS_ACCESS_KEY_ID="AKIAVYCHH6MI2XAVGEWO"\
+    # JWT_SECRET="my-super-strong-token"\
+    # URL="http://asmaa-udagram.s3-website-us-east-1.amazonaws.com"\
--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-aws s3 cp --recursive --acl public-read ./www s3://udagram-fedeval/
+aws s3 cp --recursive  ./www s3://asmaa-udagram/
